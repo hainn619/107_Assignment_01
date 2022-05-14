16e9ff7347ef1bfb47a05995952af2cc341caf41
hainn619 <101760377+hainn619@users.noreply.github.com>
2022-05-14 11:36:38 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/navbar";
+import Footer from "./Components/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Welcome to my super store!!!</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
